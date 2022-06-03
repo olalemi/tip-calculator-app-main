fc98ff7c773562671871ed12b261c788b77270b4
Nerdytoo <olaleminurudeen@gmail.com>
2022-06-03 19:34:45 +0100

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+ const Bill = document.querySelector("#Bill");
+ const SelectTips = document.querySelectorAll(".primary-btn");
+ const Custom = document.querySelector(".btn6");
+ const Npeople = document.querySelector("#Npeople");
+ const Reset = document.querySelector(".reset");
+ const TipAmount = document.querySelector(".input-label2");
+ const Total = document.querySelector(".input-label3");
+
