5b30eaba9b1738926d033330f6392937ee718660
olalemi <olaleminurudeen@gmail.com>
2022-07-03 09:38:02 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
- const Bill = document.querySelector("#Bill");
- const SelectTips = document.querySelectorAll(".primary-btn");
- const Custom = document.querySelector(".btn6");
- const Npeople = document.querySelector("#Npeople");
- const Reset = document.querySelector(".reset");
- const TipAmount = document.querySelector(".input-label2");
- const Total = document.querySelector(".input-label3");
+const bill = document.querySelector("#Bill");
+const selectTips = document.querySelectorAll(".primary-btn");
+const custom = document.querySelector('.btn6');
+const nPeople = document.querySelector('#Npeople');
+const Reset = document.querySelector('.btn7');
+const tipAmount = document.querySelector('.input-label2');
+const total = document.querySelector('.input-label3');
+
+
+// customFunction = () => {
+//     console.log("I am a boy");
+//   }
+
+// custom.addEventListener('click', customFunction())
+
+selectTips.addEventListener("click",  function() {
+console.log("Chief!!!");
+
+});
+
+custom.addEventListener(    "click", function(){
+    return "yes"
+});
+
+Reset.addEventListener("click", function() {
+  console.log('Reset');
+});
 
