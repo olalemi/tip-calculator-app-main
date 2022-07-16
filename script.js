ce72c8a07a5a9b5cc70a138c76f8d6c232630177
olalemi <olaleminurudeen@gmail.com>
2022-07-16 19:42:30 +0100

    Js update

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
-const bill = document.querySelector("#Bill");
-const selectTips = document.querySelectorAll(".primary-btn");
+const bill = document.querySelector('#Bill');
+const selectTips = document.querySelectorAll('.primary-btn');
 const custom = document.querySelector('.btn6');
 const nPeople = document.querySelector('#Npeople');
 const Reset = document.querySelector('.btn7');
 const tipAmount = document.querySelector('.input-label2');
 const total = document.querySelector('.input-label3');
+const totalBill = document.querySelector('#Tamount');
+const error = document.querySelector('.error');
 
 
-// customFunction = () => {
-//     console.log("I am a boy");
-//   }
-
-// custom.addEventListener('click', customFunction())
-
-selectTips.addEventListener("click",  function() {
-console.log("Chief!!!");
-
+bill.addEventListener('input', () => {
+  console.log('bill');
+  console.log();
 });
 
-custom.addEventListener(    "click", function(){
-    return "yes"
+selectTips.forEach((tips) => {
+  tips.addEventListener('click', () => {
+    console.log('tip');
+
+    calculate();
+   
+  });
 });
 
-Reset.addEventListener("click", function() {
+const calculate = () => {
+
+  let totalBillPerPerson = (bill.value/nPeople.value)
+  totalBill.value = totalBillPerPerson.toFixed(2)
+
+
+}
+
+nPeople.addEventListener('input', () => {
+  console.log('nPeople');
+  if (nPeople.value.toString() === '0' ){
+      error.style.visibility='visible'
+  }  else {
+    error.style.visibility='hidden'
+  }
+});
+
+custom.addEventListener('click', () => {
+  console.log('custom');
+});
+
+Reset.addEventListener('click', () => {
   console.log('Reset');
 });
 
+const print = () => {
+  console.log('olalemi');
+};
